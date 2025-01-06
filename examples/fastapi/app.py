259648dbc85da61a664c7571b066f0008b0df617
ali ugur <ali.ugur@canonical.com>
2025-01-06 08:49:47 +0300

    chore(): Modify examples to test out tracing.

--- examples/fastapi/app.py
+++ examples/fastapi/app.py
@@ -7,8 +7,13 @@ from sqlalchemy import Column, Integer, String, create_engine, inspect
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+from opentelemetry import trace
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 app = FastAPI()
 
+
+FastAPIInstrumentor.instrument_app(app)
+tracer = trace.get_tracer(__name__)
 engine = create_engine(os.environ["POSTGRESQL_DB_CONNECT_STRING"], echo=True)
 
 Session = scoped_session(sessionmaker(bind=engine))
@@ -26,7 +31,8 @@ class User(Base):
 
 @app.get("/")
 async def root():
-    return "Hello, World!"
+    with tracer.start_as_current_span("custom-span"):
+        return "Hello, World!"
 
 
 @app.get("/env/user-defined-config")
