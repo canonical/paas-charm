0fd60949b3913c523a4373143df1e6e46f84f413
Ali UĞUR <39213991+alithethird@users.noreply.github.com>
2025-04-11 10:13:35 +0300

    Update examples/expressjs/app/routes/tables.js
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- examples/expressjs/app/routes/tables.js
+++ examples/expressjs/app/routes/tables.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2025 Canonical Ltd.
+ * See LICENSE file for licensing details.
+ */
+
 var express = require('express');
 var router = express.Router();
 const db = require('../database');
