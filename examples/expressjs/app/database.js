b5331d218d29d8e95ece5d94a1a80014a4b87e6c
Ali Ugur <ali.ugur@canonical.com>
2025-04-16 14:27:03 +0300

    Chore: Fix ExpressJS example

--- examples/expressjs/app/database.js
+++ examples/expressjs/app/database.js
@@ -3,15 +3,13 @@
  * See LICENSE file for licensing details.
  */
 
-const pgp = require('pg-promise')(/* options */)
-const PG_CONNECT_STR = process.env["POSTGRESQL_DB_CONNECT_STRING"]
+const pgp = require('pg-promise')(/* options */);
+const PG_CONNECT_STR = process.env["POSTGRESQL_DB_CONNECT_STRING"];
 
+console.log("PG_CONNECT_STR", PG_CONNECT_STR);
 
-console.log("PG_CONNECT_STR", PG_CONNECT_STR)
-if (!PG_CONNECT_STR) {
-    console.error("POSTGRESQL_DB_CONNECT_STRING is not set")
-    process.exit(1)
-}
-const db = pgp(PG_CONNECT_STR)
+const db = PG_CONNECT_STR
+  ? pgp(PG_CONNECT_STR)
+  : (console.error("POSTGRESQL_DB_CONNECT_STRING is not set"), null);
 
 module.exports = db;
