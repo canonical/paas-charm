b2f728691564866339735289be4b0c1706fb742e
Ali Ugur <ali.ugur@canonical.com>
2025-05-05 09:48:22 +0300

    Fix: Fix flask minimal test

--- tests/integration/integrations/test_minimal.py
+++ tests/integration/integrations/test_minimal.py
@@ -22,9 +22,9 @@ def test_flask_minimal(
     act: Send a request to the web application.
     assert: The request succeeds.
     """
-
+    app = next(flask_minimal_app)
     status = juju.status()
-    unit_ip = status.apps[flask_minimal_app.name].units[flask_minimal_app.name + "/0"].address
+    unit_ip = status.apps[app.name].units[app.name + "/0"].address
     response = requests.get(f"http://{unit_ip}:8000/", timeout=5)
     assert "Hello" in response.text
     assert response.status_code == 200
