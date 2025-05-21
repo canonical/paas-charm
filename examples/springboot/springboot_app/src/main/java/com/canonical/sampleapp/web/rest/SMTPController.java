5179d2590660838847dd710a64f8c69926f1fafd
Yanks Yoon <yangsoo.yoon@canonical.com>
2025-05-21 15:29:51 +0000

    test: spring boot smtp test

--- examples/springboot/springboot_app/src/main/java/com/canonical/sampleapp/web/rest/SMTPController.java
+++ examples/springboot/springboot_app/src/main/java/com/canonical/sampleapp/web/rest/SMTPController.java
@@ -3,54 +3,22 @@ package com.canonical.sampleapp.web.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-class EmailRequest {
-    private String address;
-    private String subject;
-    private String body;
-
-    // Getters and Setters
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-}
-
-@RequestMapping("/smtp")
+@RequestMapping("/send_mail")
 @RestController
 public class SMTPController {
     @Autowired
     private JavaMailSender mailSender;
 
-    @PostMapping("/")
-    public String sendMail(@RequestBody EmailRequest emailRequest) {
+    @GetMapping
+    public String sendMail() {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(emailRequest.getAddress());
-        message.setSubject(emailRequest.getSubject());
-        message.setText(emailRequest.getBody());
+        message.setFrom("tester@example.com");
+        message.setTo("test@example.com");
+        message.setSubject("hello");
 
         mailSender.send(message);
         return "sent";
--- tests/integration/integrations/test_smtp.py
+++ tests/integration/integrations/test_smtp.py
@@ -24,6 +24,7 @@ logger = logging.getLogger(__name__)
         ("django_app", 8000),
         ("fastapi_app", 8080),
         ("go_app", 8080),
+        ("spring_boot_app", 8080),
     ],
 )
 async def test_smtp_integrations(
