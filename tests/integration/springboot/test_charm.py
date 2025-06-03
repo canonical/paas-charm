b8002aadbe38c3c22c7722d361b29e55f6478adc
Ali Ugur <ali.ugur@canonical.com>
2025-06-03 15:31:13 +0300

    Chore: db integration test

--- tests/integration/springboot/test_charm.py
+++ tests/integration/springboot/test_charm.py
@@ -7,7 +7,7 @@ import logging
 
 import jubilant
 import requests
-
+import pytest
 from tests.integration.types import App
 
 logger = logging.getLogger(__name__)
@@ -25,6 +25,29 @@ def test_springboot_is_up(spring_boot_app: App, juju: jubilant.Juju):
     assert status.apps[spring_boot_app.name].units[spring_boot_app.name + "/0"].is_active
     for unit in status.apps[spring_boot_app.name].units.values():
         assert unit.is_active, f"Unit {unit.name} is not active"
-        response = requests.get(f"http://{unit.address}:{WORKLOAD_PORT}", timeout=5)
+        response = requests.get(f"http://{unit.address}:{WORKLOAD_PORT}/hello-world", timeout=5)
         assert response.status_code == 200
         assert "Hello, World!" in response.text
+
+
+def test_migration(spring_boot_app: App, request: pytest.FixtureRequest, juju: jubilant.Juju):
+    """
+    arrange: build and deploy the Springboot charm with postgresql integration.
+    act: send a request to an endpoint that checks the table created by the migration script.
+        Then try to add same user twice.
+    assert: the Springboot application should add the user only once.
+    """
+    status = juju.status()
+    for unit in status.apps[spring_boot_app.name].units.values():
+        response = requests.get(f"http://{unit.address}:{WORKLOAD_PORT}/table/users", timeout=5)
+        assert response.status_code == 200
+        assert "SUCCESS" in response.text
+        user_creation_request = {"name": "foo", "password": "bar"}
+        response = requests.post(
+            f"http://{unit.address}:8080/users", json=user_creation_request, timeout=5
+        )
+        assert response.status_code == 201
+        response = requests.post(
+            f"http://{unit.address}:8080/users", json=user_creation_request, timeout=5
+        )
+        assert response.status_code == 400
