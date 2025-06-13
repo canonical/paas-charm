9898e59f4500e95c98a1ee51f45ca5aa28c60ae3
Ali Ugur <ali.ugur@canonical.com>
2025-06-13 12:55:16 +0300

    Chore: Fix Springboot saml

--- examples/springboot/spring-boot_app/src/main/java/com/canonical/sampleapp/config/NoSecurityConfiguration.java
+++ examples/springboot/spring-boot_app/src/main/java/com/canonical/sampleapp/config/NoSecurityConfiguration.java
@@ -6,6 +6,7 @@
 package com.canonical.sampleapp.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +14,11 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @ConditionalOnMissingBean(SecurityFilterChain.class)
+@ConditionalOnProperty(
+    name = "spring.security.saml2.relyingparty.registration.testentity.entity-id",
+    matchIfMissing = true,
+    havingValue = "false"
+)
 public class NoSecurityConfiguration {
     @Bean
     public SecurityFilterChain permitAllSecurityFilterChain(HttpSecurity http) throws Exception {
