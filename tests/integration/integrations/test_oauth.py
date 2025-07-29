cede072d2b84b217d9c35621524f911a6f2d34aa
Ali Ugur <ali.ugur@canonical.com>
2025-07-29 12:10:47 +0300

    Chore: Fix oauth test

--- tests/integration/integrations/test_oauth.py
+++ tests/integration/integrations/test_oauth.py
@@ -112,7 +112,7 @@ def _admin_identity_exists(juju, test_email):
 
 def _assert_idp_login_success(app_url: str, endpoint: str, test_email: str, test_password: str):
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         context = browser.new_context(ignore_https_errors=True)
         page = context.new_page()
         page.goto(f"{app_url}/{endpoint}")
