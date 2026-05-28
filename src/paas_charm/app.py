2216a49d59b2914680648a12d7e772fc0d6635a8
tphan025 <trung.thanh.phan@canonical.com>
2026-05-28 18:13:03 +0200

    Revert "append "{prefix}://" to db env vars if not already present"
    This reverts commit 7d5d1235dafc014e463f237887508fda660bfbc2.

--- src/paas_charm/app.py
+++ src/paas_charm/app.py
@@ -589,9 +589,9 @@ class App:  # pylint: disable=too-many-instance-attributes
         services[self._workload_config.service_name]["override"] = "replace"
         services[self._workload_config.service_name]["environment"] = self.gen_environment()
         if self._alternate_service_command:
-            services[self._workload_config.service_name]["command"] = (
-                self._alternate_service_command
-            )
+            services[self._workload_config.service_name][
+                "command"
+            ] = self._alternate_service_command
 
         for service_name, service in services.items():
             normalised_service_name = service_name.lower()
@@ -657,9 +657,6 @@ def _url_env_vars(prefix: str, url: str) -> dict[str, str]:
     """
     if not url:
         return {}
-
-    if not url.startswith(f"{prefix.lower()}://"):
-        url = f"{prefix.lower()}://{url}"
 
     envvars: dict[str, str | None] = {}
     envvars[f"{prefix}_CONNECT_STRING"] = url
