3b71b2e77228a899b4e947ce59e600ac85ac515b
Ali Ugur <aliugur997@gmail.com>
2026-06-05 14:11:51 +0300

    fix: add license header and --ignore-installed to pre-build script
    - Add Canonical copyright header to pass license-eye checks
    - Add --ignore-installed to pip install so it doesn't try to
      uninstall system-installed urllib3 on GitHub runners

--- scripts/use-rockcraft-fork.sh
+++ scripts/use-rockcraft-fork.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
 # Install rockcraft from alithethird fork (feat/bump-gunicorn-statsd).
 # Installs to /usr/local/bin which precedes /snap/bin in PATH on GitHub runners,
 # so this version is used for rock builds even after the snap install step runs.
@@ -6,7 +8,9 @@ set -ex
 
 FORK_URL="git+https://github.com/alithethird/rockcraft@feat/bump-gunicorn-statsd"
 
-sudo pip install --break-system-packages "${FORK_URL}"
+sudo pip install --break-system-packages \
+  --ignore-installed \
+  "${FORK_URL}"
 
 # Verify the installed version is from our fork
 rockcraft --version || /usr/local/bin/rockcraft --version
