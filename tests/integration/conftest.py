9db3a2029a56742b869a750457e9df31b9d2ddfe
Ali Ugur <aliugur997@gmail.com>
2026-06-19 14:29:59 +0300

    fix: use concierge-k8s

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -667,16 +667,15 @@ def juju(request: pytest.FixtureRequest) -> jubilant.Juju:
     use_existing = request.config.getoption("--use-existing", default=False)
     if use_existing:
         juju = jubilant.Juju()
-        juju.cli(["switch", "concierge-k8s"])
         return juju
 
     model = request.config.getoption("--model")
     if model:
-        juju = jubilant.Juju(model=f"concierge-k8s:{model}")
+        juju = jubilant.Juju(model=model)
         return juju
 
     keep_models = cast(bool, request.config.getoption("--keep-models"))
-    with jubilant.temp_model(controller="concierge-k8s", keep=keep_models) as juju:
+    with jubilant.temp_model(keep=keep_models) as juju:
         juju.wait_timeout = 10 * 60
         return juju
 
