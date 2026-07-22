481232000d995318edbdc8e9e7826b2e4e3fe6a1
Ali Ugur <aliugur997@gmail.com>
2026-07-22 11:32:55 +0300

    fix: change port order.

--- tests/integration/expressjs/test_expressjs.py
+++ tests/integration/expressjs/test_expressjs.py
@@ -32,23 +32,6 @@ def test_expressjs_is_up(expressjs_app: App, request: pytest.FixtureRequest, juj
         assert "Hello, World!" in response.text
 
 
-def test_expressjs_port(expressjs_app: App, request: pytest.FixtureRequest, juju: jubilant.Juju):
-    """
-    arrange: build and deploy the ExpressJS charm.
-    act: call the endpoint.
-    assert: the charm should respond with 200 OK.
-    """
-    status = juju.status()
-    juju.config(expressjs_app.name, {"app-port": TEST_PORT})
-    juju.wait(lambda status: jubilant.all_active(status, expressjs_app.name, "postgresql-k8s"))
-    assert status.apps[expressjs_app.name].units[expressjs_app.name + "/0"].is_active
-    for unit in status.apps[expressjs_app.name].units.values():
-        assert unit.is_active, f"Unit {unit.name} is not active"
-        response = requests.get(f"http://{unit.address}:{TEST_PORT}", timeout=5)
-        assert response.status_code == 200
-        assert "Hello, World!" in response.text
-
-
 def test_user_defined_config(
     expressjs_app: App, request: pytest.FixtureRequest, juju: jubilant.Juju
 ):
@@ -91,3 +74,20 @@ def test_migration(expressjs_app: App, request: pytest.FixtureRequest, juju: jub
             f"http://{unit.address}:8080/users", json=user_creation_request, timeout=5
         )
         assert response.status_code == 400
+
+
+def test_expressjs_port(expressjs_app: App, request: pytest.FixtureRequest, juju: jubilant.Juju):
+    """
+    arrange: build and deploy the ExpressJS charm.
+    act: call the endpoint.
+    assert: the charm should respond with 200 OK.
+    """
+    status = juju.status()
+    juju.config(expressjs_app.name, {"app-port": TEST_PORT})
+    juju.wait(lambda status: jubilant.all_active(status, expressjs_app.name, "postgresql-k8s"))
+    assert status.apps[expressjs_app.name].units[expressjs_app.name + "/0"].is_active
+    for unit in status.apps[expressjs_app.name].units.values():
+        assert unit.is_active, f"Unit {unit.name} is not active"
+        response = requests.get(f"http://{unit.address}:{TEST_PORT}", timeout=5)
+        assert response.status_code == 200
+        assert "Hello, World!" in response.text
