22cde311cff25bcc18bbc42a2011da55354c949c
Dmitry Malykhanov <dmitry.malykhanov@canonical.com>
2026-08-27 13:39:48 +0100

    feat: add custom relations extension API
    Expose a public extension point that lets charm authors plug their own Juju
    relations into the paas-charm lifecycle.

--- src/paas_charm/charm_state.py
+++ src/paas_charm/charm_state.py
@@ -17,6 +17,7 @@ from paas_charm.exceptions import (
     RelationDataError,
 )
 from paas_charm.peers import Peers
+from paas_charm.relations import CustomRelation
 from paas_charm.secret_key import SecretKeyStorage
 from paas_charm.utils import build_validation_error_message, config_metadata
 
@@ -62,6 +63,7 @@ class CharmState:  # pylint: disable=too-many-instance-attributes
         peer_fqdns: str | None = None,
         integrations: "IntegrationsState | None" = None,
         base_url: str | None = None,
+        custom_relations: list[CustomRelation] | None = None,
     ):
         """Initialize a new instance of the CharmState class.
 
@@ -74,6 +76,7 @@ class CharmState:  # pylint: disable=too-many-instance-attributes
             peer_fqdns: The FQDN of units in the peer relation.
             integrations: Information about the integrations.
             base_url: Base URL for the service.
+            custom_relations: Custom relations.
         """
         self.framework = framework
         self._framework_config = framework_config if framework_config is not None else {}
@@ -83,6 +86,7 @@ class CharmState:  # pylint: disable=too-many-instance-attributes
         self.peer_fqdns = peer_fqdns
         self.integrations = integrations or IntegrationsState()
         self.base_url = base_url
+        self.custom_relations = custom_relations or []
 
     @classmethod
     def from_charm(  # pylint: disable=too-many-arguments,too-many-locals
@@ -210,6 +214,7 @@ class CharmState:  # pylint: disable=too-many-instance-attributes
                 f"{exc.relation} relation data is either unavailable, invalid or not usable.",
                 relation=exc.relation,
             ) from exc
+
         peer_fqdns = None
         if peers.is_related and (peer_unit_fqdns := peers.get_peer_unit_fqdns()):
             peer_fqdns = ",".join(peer_unit_fqdns)
@@ -225,6 +230,7 @@ class CharmState:  # pylint: disable=too-many-instance-attributes
             peer_fqdns=peer_fqdns,
             integrations=integrations,
             base_url=base_url,
+            custom_relations=integration_requirers.custom_relations,
         )
 
     @property
@@ -307,6 +313,7 @@ class IntegrationRequirers:  # pylint: disable=too-many-instance-attributes
         openfga: OpenFGA requirer object.
         oauth: PaaSOAuthRequirer object.
         http_proxy: PaaSHttpProxyRequirer object.
+        custom_relations: Tuple of instantiated custom relations.
     """
 
     databases: dict[str, "PaaSDatabaseRequires"]
@@ -319,6 +326,7 @@ class IntegrationRequirers:  # pylint: disable=too-many-instance-attributes
     smtp: "SmtpRequires | None" = None
     oauth: "PaaSOAuthRequirer | None" = None
     http_proxy: "PaaSHttpProxyRequirer | None" = None
+    custom_relations: tuple["CustomRelation", ...] = ()
 
 
 @dataclass
